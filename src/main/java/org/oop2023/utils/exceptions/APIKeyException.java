d4201f5a43a18031fcdbe9d3637235f633eb2b85
kiennguyen246 <duckienntt1@gmail.com>
2023-11-12 16:42:58 +0700

    Added translate API.

---
+++ src/main/java/org/oop2023/utils/exceptions/APIKeyException.java
@@ -0,0 +1,11 @@
+package org.oop2023.utils.exceptions;
+
+public class APIKeyException extends APIException {
+    /**
+     * Default constructor.
+     */
+    public APIKeyException(String message) {
+        super(message);
+    }
+
+}
